1c602178ee4a50d8c247004b68bf0aabb05ca5c6
Andrew T <me@andrewtran-shi.dev>
2023-05-02 12:08:29 -0700

    added labels

--- bankAccount.py
+++ bankAccount.py
@@ -1,5 +1,5 @@
 # Create a BankAccount class with the attributes interest rate and balance
-class BankAccount:
+class bankAccount:
 
     # don't forget to add some default values for these parameters!
     def __init__(self, int_rate, balance=0):
@@ -7,12 +7,12 @@ class BankAccount:
         self.balance = balance
         # your code here! (remember, instance attributes go here)
         # don't worry about user info here; we'll involve the User class soon
-# Add a deposit method to the BankAccount class
+# Add a deposit method to the bankAccount class
 
     def deposit(self, amount):
         self.balance += amount
         return self
-# Add a withdraw method to the BankAccount class
+# Add a withdraw method to the bankAccount class
 
     def withdraw(self, amount):
         self.balance -= amount
@@ -20,13 +20,13 @@ class BankAccount:
             print("Insufficient funds: Charging a $5 fee")
         self.balance -= 5
         return self
-# Add a display_account_info method to the BankAccount class
+# Add a display_account_info method to the bankAccount class
 
     def display_account_info(self):
-        print(self.balance)
+        print("Balance: " + str(self.balance))
         return self
 
-# Add a yield_interest method to the BankAccount class
+# Add a yield_interest method to the bankAccount class
     def yield_interest(self):
         if self.balance > 0:
             self.balance *= (1 + self.int_rate)
@@ -34,8 +34,8 @@ class BankAccount:
 
 
 # Create 2 accounts
-account1 = BankAccount(.10, 200)  # interest 10% with 200 initial balance
-account2 = BankAccount(.05, 100)  # interest 5% with 100 initial balance
+account1 = bankAccount(.10, 200)  # interest 10% with 200 initial balance
+account2 = bankAccount(.05, 100)  # interest 5% with 100 initial balance
 
 #To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in one line of code (i.e. chaining)
 
--- users_with_bankAccount.py
+++ users_with_bankAccount.py
@@ -1,10 +1,33 @@
+from bankAccount import bankAccount
+
 class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.account = bankAccount(int_rate=0.02, balance=0)
 
     # other methods
 
+# Add a make_deposit method to the User class that calls on it's bank account's instance methods.
+
     def make_deposit(self, amount):
-    	# your code here
+        self.account.balance += amount
+        return self
+
+# Add a withdraw method to the BankAccount class
+
+    def make_withdraw(self, amount):
+        self.account.balance -= amount
+        if self.account.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+        self.account.balance -= 5
+        return self.account.balance
+
+# Add a display_user_balance method to the User class that displays user's account balance
+
+    def display_account_balance(self):
+        print(self.account.balance)
+        return self
+
+account3 = User("Andrew", "Andrew@Tran.com")
+account3.make_deposit(100000).make_deposit(100000).display_account_balance()
